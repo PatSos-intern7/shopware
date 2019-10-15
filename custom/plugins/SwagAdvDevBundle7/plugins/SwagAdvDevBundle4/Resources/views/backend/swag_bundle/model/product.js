afcebac0029a7769f450968285f2371ea23efbfb
Patrycjusz Sosnowski <intern7@wearevirtua.com>
2019-10-15 13:08:00 +0200

    removing unnecessarily plugins

--- custom/plugins/SwagAdvDevBundle7/plugins/SwagAdvDevBundle4/Resources/views/backend/swag_bundle/model/product.js
+++
@@ -1,9 +0,0 @@
-Ext.define('Shopware.apps.SwagBundle.model.Product', {
-    extend: 'Shopware.apps.Base.model.Article',
-
-    configure: function() {
-        return {
-            related: 'Shopware.apps.SwagBundle.view.detail.Product'
-        };
-    }
-});
