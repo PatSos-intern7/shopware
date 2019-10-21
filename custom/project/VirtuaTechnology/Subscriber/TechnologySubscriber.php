12f700c294c6b0f2381c9ff0b791e7e8f9c8be96
Patrycjusz Sosnowski <intern7@wearevirtua.com>
2019-10-21 13:51:50 +0200

    add get data for technology tab.

--- custom/project/VirtuaTechnology/Subscriber/TechnologySubscriber.php
+++ custom/project/VirtuaTechnology/Subscriber/TechnologySubscriber.php
@@ -3,6 +3,7 @@ namespace VirtuaTechnology\Subscriber;
 use Enlight\Event\SubscriberInterface;
 use Enlight_Controller_ActionEventArgs;
 use Enlight_Event_EventArgs;
+use Shopware\Models\Shop\Shop;
 
 class TechnologySubscriber implements SubscriberInterface
 {
@@ -26,13 +27,50 @@ class TechnologySubscriber implements SubscriberInterface
     public static function getSubscribedEvents()
     {
         return [
-            'Enlight_Controller_Action_PreDispatch_Frontend_Technology' => 'registerViewDirectory'
+            'Enlight_Controller_Action_PreDispatch_Frontend' => 'registerViewDirectory',
+            'Enlight_Controller_Action_PreDispatch_Frontend_Detail' =>'technologyTab'
+
         ];
     }
     public function registerViewDirectory(Enlight_Controller_ActionEventArgs $args)
     {
         $pluginBasePath = Shopware()->Container()->getParameter('virtua_technology.plugin_dir');
         $this->templateManager->addTemplateDir($pluginBasePath.'/Resources/views');
+        $this->templateManager->addTemplateDir($pluginBasePath.'/Resources/snippets');
+    }
 
+    public function technologyTab(Enlight_Controller_ActionEventArgs $args)
+    {
+        $technologyService = Shopware()->Container()->get('virtua_technology_services.technology_service');
+        $articlesApi = Shopware()->Container()->get('shopware.api.article');
+
+        $articleId = $args->getRequest()->getParam('sArticle');
+        $oneArticle = $articlesApi->getOne($articleId);
+
+        $technologyList = $this->prepTechnologyTabData($oneArticle, $technologyService);
+
+        $this->templateManager->assign('techList',$technologyList);
+    }
+
+    /**
+     * @param $oneArticle
+     * @param $techService
+     * @return array
+     */
+    private function prepTechnologyTabData($oneArticle, $techService): array
+    {
+        $techs = $oneArticle['mainDetail']['attribute']['technology'];
+        if($techs) {
+            $techArray = explode('|', $techs);
+            $techArray = array_filter($techArray);
+
+            $technologyList = [];
+            foreach ($techArray as $tech) {
+                $technologyList[] = $techService->getTechnology($tech);
+            }
+        } else {
+            $technologyList = [];
+        }
+        return $technologyList;
     }
 }
