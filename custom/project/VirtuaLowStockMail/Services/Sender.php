1acc161bc6771567c34876d8b9f725dd97c193f9
Patrycjusz Sosnowski <intern7@wearevirtua.com>
2019-10-24 15:19:08 +0200

    add content to sender

--- custom/project/VirtuaLowStockMail/Services/Sender.php
+++ custom/project/VirtuaLowStockMail/Services/Sender.php
@@ -4,10 +4,11 @@ namespace VirtuaLowStockMail\Services;
 
 class Sender
 {
-    public function sendMail(string $templateName, string $sendTo): void
+    public function sendMail(string $templateName, string $sendTo, string $contentHtml): void
     {
         $mail = Shopware()->TemplateMail()->createMail($templateName);
         $mail->addTo($sendTo);
+        $mail->setBodyHtml($contentHtml);
         $mail->send();
     }
 }
