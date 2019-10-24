2f263a5177ca2b235651e77fdcc3405e946be933
Patrycjusz Sosnowski <intern7@wearevirtua.com>
2019-10-24 11:24:11 +0200

    update sendMail in sender.

--- custom/project/VirtuaLowStockMail/Services/Sender.php
+++ custom/project/VirtuaLowStockMail/Services/Sender.php
@@ -4,10 +4,10 @@ namespace VirtuaLowStockMail\Services;
 
 class Sender
 {
-    public function sendMail(string $templateName): void
+    public function sendMail(string $templateName, string $sendTo): void
     {
         $mail = Shopware()->TemplateMail()->createMail($templateName);
-        $mail->addTo('mail@gmail.com');
+        $mail->addTo($sendTo);
         $mail->send();
     }
 }
